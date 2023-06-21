1c2e9324be85dedd3cca427c92390603bf3fbc38
nhat_minh <hnnminh24112000@gmail.com>
2023-06-21 17:14:52 +0700

    handle-dnd

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,125 +1,17 @@
 
 <script setup>
-import { ref, computed } from "vue";
+import { Timestamp } from "firebase/firestore";
+import { ref, computed, onMounted } from "vue";
 import draggable from "vuedraggable";
 import Ticket from "../components/Ticket.vue";
+import store from "../store"
 
+const lists = computed(()=>store.state.listEvents.data)
+const isLoadingListEvents = computed(()=>store.state.listEvents.isLoading)
 
-const lanes = ref([
-	{
-		name: "To Do",
-		tickets: [
-			{
-				title: "We don't have a brig. Meh. Calculon is gonna kill us and it's all everybody else's fault!",
-				author: "Philip J. Fry",
-				created_at: "16 hours ago",
-				level: "Medium Level",
-				comments_count: 12,
-			},
-			{
-				title: "This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel.",
-				author: "Turanga Leela",
-				created_at: "16 hours ago",
-				level: "High Level",
-				comments_count: 1,
-			},
-			{
-				title: "Stop it, stop it. It's fine. I will 'destroy' you! I can explain. It's very valuable. ",
-				author: "Bender Bending Rodriguez",
-				created_at: "16 hours ago",
-				level: "Low Level",
-				comments_count: 12,
-			},
-			{
-				title: "Hey, whatcha watching? Hey! I'm a porno-dealing monster, what do I care what you think? It must be wonderful.",
-				author: "Professor Farnsworth",
-				created_at: "16 hours ago",
-				level: "Medium Level",
-				comments_count: 0,
-			},
-			{
-				title: "A superpowers drug you can just rub onto your skin? You'd think it would be something you'd have to freebase.",
-				author: "Amy Wong",
-				created_at: "16 hours ago",
-				level: "High Level",
-				comments_count: 56,
-			},
-			{
-				title: "Robot 1-X, save my friends! And Zoidberg! Perhaps, but perhaps your civilization is merely the sewer of an even greater society above you!",
-				author: "Hermes Conrad",
-				created_at: "16 hours ago",
-				level: "Medium Level",
-				comments_count: 10,
-			},
-			{
-				title: "You are the last hope of the universe. Stop! Don't shoot fire stick in space canoe!",
-				author: "Dr. John A. Zoidberg",
-				created_at: "16 hours ago",
-				level: "Low Level",
-				comments_count: 3,
-			},
-		],
-	},
-	{
-		name: "In Progress",
-		tickets: [
-			{
-				title: "You are the last hope of the universe. Stop! Don't shoot fire stick in space canoe!",
-				author: "Dr. John A. Zoidberg",
-				created_at: "16 hours ago",
-				level: "Low Level",
-				comments_count: 12,
-			},
-			{
-				title: "A superpowers drug you can just rub onto your skin? You'd think it would be something you'd have to freebase.",
-				author: "Amy Wong",
-				created_at: "16 hours ago",
-				level: "High Level",
-				comments_count: 2,
-			},
-			{
-				title: "This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel.",
-				author: "Turanga Leela",
-				created_at: "16 hours ago",
-				level: "High Level",
-				comments_count: 12,
-			},
-		],
-	},
-	{
-		name: "Done",
-		tickets: [
-			{
-				title: "Stop it, stop it. It's fine. I will 'destroy' you! I can explain. It's very valuable. ",
-				author: "Bender Bending Rodriguez",
-				created_at: "16 hours ago",
-				level: "Low Level",
-				comments_count: 12,
-			},
-			{
-				title: "Hey, whatcha watching? Hey! I'm a porno-dealing monster, what do I care what you think? It must be wonderful.",
-				author: "Professor Farnsworth",
-				created_at: "16 hours ago",
-				level: "Medium Level",
-				comments_count: 0,
-			},
-			{
-				title: "We don't have a brig. Meh. Calculon is gonna kill us and it's all everybody else's fault!",
-				author: "Philip J. Fry",
-				created_at: "16 hours ago",
-				level: "Medium Level",
-				comments_count: 12,
-			},
-			{
-				title: "This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel.",
-				author: "Turanga Leela",
-				created_at: "16 hours ago",
-				level: "High Level",
-				comments_count: 1,
-			},
-		],
-	},
-]);
+onMounted(async()=>{
+	store.dispatch("getListEvents")
+})
 
 const dragOptions = computed(() => {
 	return {
@@ -129,12 +21,39 @@ const dragOptions = computed(() => {
 	};
 });
 
-const handleChange = (e,value)=>{console.log({from:e.from.getAttribute("data-id")});console.log({to:e.to.getAttribute("data-id")}); console.log(value)}
+function handleChange(e,listEvents=null){
+	// console.log(listEvents.find(list=>list.label_status === e.to.getAttribute("data-id")).events[e.newIndex])
+	// console.log({from:e.from.getAttribute("data-id")});console.log({to:e.to.getAttribute("data-id")}); console.log({e})
+	// console.log({...changedEvent, status: e.to.getAttribute("data-id")})
+	console.log({e})
+	const changedEvent = listEvents.find(list=>list.label_status === e.to.getAttribute("data-id")).events[e.newIndex]
+	console.log({changedEvent})
+
+	const params = {
+		...changedEvent,
+		status: e.to.getAttribute("data-id"),
+		actions: [{
+			description: `Minh pro changed the status to ${e.to.getAttribute("data-id")}`,
+			type:'',
+			createdAt: Timestamp.now()
+		},...changedEvent.actions]
+	}
+	console.log({params})
+	store.dispatch("updateListEvents",params)
+}
+
+
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-6">
-    <div v-for="lane in lanes" :key="lane.name" class="border border-gray-300 rounded-md bg-gray-50">
+  <!-- loading -->
+  <div v-if="isLoadingListEvents">
+    Loading...
+  </div>
+
+  <!-- content  -->
+  <div v-else class="grid grid-cols-4 gap-3">
+    <div v-for="list in lists" :key="list.label_status" class="border border-gray-300 rounded-md bg-gray-50">
       <div class="
 					bg-white
 					border-b border-gray-300
@@ -145,13 +64,13 @@ const handleChange = (e,value)=>{console.log({from:e.from.getAttribute("data-id"
 					justify-between
 				">
         <div class="text-lg font-semibold">
-          {{ lane.name }}
+          {{ list.label_status }}
         </div>
 
         <div class="flex items-center space-x-4">
-          <button v-if="lane.name == 'Done'" class="text-blue-500 hover:text-blue-700 font-semibold">
+          <!-- <button v-if="list.label_status == 'done'" class="text-blue-500 hover:text-blue-700 font-semibold">
             Clear all
-          </button>
+          </button> -->
 
           <span class="
 							block
@@ -162,13 +81,13 @@ const handleChange = (e,value)=>{console.log({from:e.from.getAttribute("data-id"
 							text-sm
 							font-semibold
 						">
-            {{ lane.tickets.length }}
+            {{ list.events.length }}
           </span>
         </div>
       </div>
 
       <div class="p-4 h-full">
-        <draggable :data-id="lane.name" @end="(event)=>handleChange(event,lanes)" class="min-h-full" :list="lane.tickets" group="tickets" itemKey="name" v-bind="dragOptions">
+        <draggable :data-id="list.label_status" @end="(event)=>handleChange(event, lists)" class="min-h-full" :list="list.events" group="tickets" itemKey="name" v-bind="dragOptions">
           <template #item="{ element }">
             <Ticket :ticket="element" />
           </template>
